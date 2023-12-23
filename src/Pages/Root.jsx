743a6b9269a3e871f12596473ea8d134ebbccc7c
teethw <kazisadman01@gmail.com>
2023-12-23 22:31:29 +0600

    Added footer

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Shared/Navbar";
+import Footer from "../Components/Shared/Footer";
 
 const Root = () => {
   return (
     <div className="font-poppins max-w-6xl mx-auto px-3 lg:px-0">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
